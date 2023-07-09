f2d9c6fa5a4afb6f5bb1d200cdfcc2f4c635969b
Felipe Faria <felipevisu@gmail.com>
2023-07-09 18:43:36 -0300

    first commit

---
+++ djangogql/core/types/__init__.py
@@ -0,0 +1,5 @@
+from .common import *
+from .filter_input import *
+from .model import *
+from .sort_input import *
+from .upload import *
---
+++ djangogql/middleware.py
@@ -0,0 +1,59 @@
+import logging
+from datetime import datetime
+
+from django.conf import settings
+from django.contrib.auth import authenticate, get_user_model
+from django.contrib.auth.models import AnonymousUser
+from django.utils.functional import SimpleLazyObject
+
+from .jwt import JWT_REFRESH_TOKEN_COOKIE_NAME, jwt_decode_with_exception_handler
+from .settings import djangogql_settings
+
+User = get_user_model()
+
+Requestor = "User"
+logger = logging.getLogger(__name__)
+
+
+def jwt_refresh_token_middleware(get_response):
+    def middleware(request):
+        """Append generated refresh_token to response object."""
+        response = get_response(request)
+        jwt_refresh_token = getattr(request, "refresh_token", None)
+        if jwt_refresh_token:
+            expires = None
+            secure = not settings.DEBUG
+            if djangogql_settings.JWT_EXPIRE:
+                refresh_token_payload = jwt_decode_with_exception_handler(
+                    jwt_refresh_token
+                )
+                if refresh_token_payload and refresh_token_payload.get("exp"):
+                    expires = datetime.utcfromtimestamp(refresh_token_payload["exp"])
+            response.set_cookie(
+                JWT_REFRESH_TOKEN_COOKIE_NAME,
+                jwt_refresh_token,
+                expires=expires,
+                httponly=True,  # protects token from leaking
+                secure=secure,
+                samesite="None" if secure else "Lax",
+            )
+        return response
+
+    return middleware
+
+
+def get_user(request):
+    if not hasattr(request, "_cached_user"):
+        request._cached_user = authenticate(request=request)
+    return request._cached_user
+
+
+class JWTMiddleware:
+    def resolve(self, next, root, info, **kwargs):
+        request = info.context
+
+        def user():
+            return get_user(request) or AnonymousUser()
+
+        request.user = SimpleLazyObject(lambda: user())
+        return next(root, info, **kwargs)
