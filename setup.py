e3e1f03874dafef064ae09b3ca76695588264312
Felipe Faria <felipevisu@gmail.com>
2023-07-12 20:57:21 -0300

    chore: bump version to 1.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="python-djangogql",
-    version="1.3.1",
+    version="1.4.0",
     description="A Django app to build graphql APIs",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
